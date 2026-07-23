778cc6502aa08d55f09f14e87a238590ae78af13
Yusuf Bhatti <yusuf.bhatti@hotmail.com>
2026-07-23 18:48:53 +0200

    added new MM PPE files

--- MM_PPE_ECHAM/my_experiments/Jasper_2006/echam_jobscript_Jasper_2006.sh
+++ MM_PPE_ECHAM/my_experiments/Jasper_2006/echam_jobscript_Jasper_2006.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name="PPE_TEST"
+#SBATCH --job-name="Jasper_2006"
 #SBATCH --nodes=1
 #SBATCH --ntasks=128
 #SBATCH --time=14:00:00
 #SBATCH --partition genoa
-###SBATCH --output=slurm_PPE_TEST_%j.txt
-###SBATCH --error=slurm_PPE_TEST_%j.txt
-#SBATCH --output=/home/ybhatti2/prjs1474/Pace_PPE_Output/MM_PPE_Experiments/PPE_TEST/PPE_TEST.out
-#SBATCH --error=/home/ybhatti2/prjs1474/Pace_PPE_Output/MM_PPE_Experiments/PPE_TEST/PPE_TEST.err
+###SBATCH --output=slurm_Jasper_2006_%j.txt
+###SBATCH --error=slurm_Jasper_2006_%j.txt
+#SBATCH --output=/home/ybhatti2/prjs1474/Pace_PPE_Output/MM_PPE_Experiments/Jasper_2006/Jasper_2006.out
+#SBATCH --error=/home/ybhatti2/prjs1474/Pace_PPE_Output/MM_PPE_Experiments/Jasper_2006/Jasper_2006.err
 #SBATCH --account="srs25001"
 #!/bin/bash -l
 
@@ -35,7 +35,7 @@ export MPICH_UNEX_BUFFER_SIZE=
 #----------------------------------------------------------------------------------------
 #--- Start the run:
 
-cd /home/ybhatti2/prjs1474/Pace_PPE_Output/MM_PPE_Experiments/PPE_TEST/
+cd /home/ybhatti2/prjs1474/Pace_PPE_Output/MM_PPE_Experiments/Jasper_2006/
 
 set +e
 srun --exclusive --ntasks=128 ./echam6
@@ -53,46 +53,46 @@ set -e
 #--- Launch some p-proc job here:
 
 ### Add my CDO command here for P-processing the TAU 3D into 2D (vert int) modes
-#cwd=/home/ybhatti2/prjs1474/Pace_PPE_Output/MM_PPE_Experiments/PPE_TEST/
+#cwd=/home/ybhatti2/prjs1474/Pace_PPE_Output/MM_PPE_Experiments/Jasper_2006/
 ## Copy the CDO.sh template to a new script based on jobname
-#cp -f /home/ybhatti2/prjs1474/Utils/CDO_Scripts/PPE_ENS_Example.sh /home/ybhatti2/prjs1474/Utils/CDO_Scripts/CDO_PP/PPE_TEST.sh
+#cp -f /home/ybhatti2/prjs1474/Utils/CDO_Scripts/PPE_ENS_Example.sh /home/ybhatti2/prjs1474/Utils/CDO_Scripts/CDO_PP/Jasper_2006.sh
 #
 ## Replace line 6 with the exp_dir variable value
-##sed -i '6c\ name='"~/prjs1076/PPE_Output/PPE_Experiments/PPE_TEST/" ~/prjs1076/yusufb/Scripts/CDO_PP_PI/PPE_TEST.sh
-#sed -i "7c\ name=\"/home/ybhatti2/prjs1474/Pace_PPE_Output/PPE_Experiments/PPE_TEST/\"" /home/ybhatti2/prjs1474/Utils/CDO_Scripts/CDO_PP/PPE_TEST.sh
+##sed -i '6c\ name='"~/prjs1076/PPE_Output/PPE_Experiments/Jasper_2006/" ~/prjs1076/yusufb/Scripts/CDO_PP_PI/Jasper_2006.sh
+#sed -i "7c\ name=\"/home/ybhatti2/prjs1474/Pace_PPE_Output/PPE_Experiments/Jasper_2006/\"" /home/ybhatti2/prjs1474/Utils/CDO_Scripts/CDO_PP/Jasper_2006.sh
 #
 ## Replace line 14 with the jobname variable value
-#sed -i "6c\ jobname='PPE_TEST'" /home/ybhatti2/prjs1474/Utils/CDO_Scripts/CDO_PP/PPE_TEST.sh 
-#sed -i "6c\mv -f ~/prjs1076/PPE_Output/Pace_PPE_Experiments/PPE_TEST /archive/ybhatti1/Pace_PPE/PPE_Experiments/" ~/Scripts/CDO_PP/PPE_TEST.sh
+#sed -i "6c\ jobname='Jasper_2006'" /home/ybhatti2/prjs1474/Utils/CDO_Scripts/CDO_PP/Jasper_2006.sh 
+#sed -i "6c\mv -f ~/prjs1076/PPE_Output/Pace_PPE_Experiments/Jasper_2006 /archive/ybhatti1/Pace_PPE/PPE_Experiments/" ~/Scripts/CDO_PP/Jasper_2006.sh
 
 # -----------------------------------------------------
 
 # Define target script once
-target="/home/ybhatti2/prjs1474/Utils/CDO_Scripts/CDO_PP_PI/PPE_TEST.sh"
+target="/home/ybhatti2/prjs1474/Utils/CDO_Scripts/CDO_PP_PI/Jasper_2006.sh"
 
 # Define paths for reuse
-output_dir="/home/ybhatti2/prjs1474/Pace_PPE_Output/PPE_Experiments/PPE_TEST/"
-archive_dir="~/prjs1076/PPE_Output/Pace_PPE_Experiments/PPE_TEST"
+output_dir="/home/ybhatti2/prjs1474/Pace_PPE_Output/PPE_Experiments/Jasper_2006/"
+archive_dir="~/prjs1076/PPE_Output/Pace_PPE_Experiments/Jasper_2006"
 
 # Copy the CDO.sh template to a new script based on jobname
-#cp -f /home/ybhatti2/prjs1474/Utils/CDO_Scripts/PPE_ENS_Example.sh /home/ybhatti2/prjs1474/Utils/CDO_Scripts/CDO_PP/PPE_TEST.sh
-cp -f /home/ybhatti2/prjs1474/Utils/CDO_Scripts/PPE_ENS_Defaults_PI.sh /home/ybhatti2/prjs1474/Utils/CDO_Scripts/CDO_PP_PI/PPE_TEST.sh
+#cp -f /home/ybhatti2/prjs1474/Utils/CDO_Scripts/PPE_ENS_Example.sh /home/ybhatti2/prjs1474/Utils/CDO_Scripts/CDO_PP/Jasper_2006.sh
+cp -f /home/ybhatti2/prjs1474/Utils/CDO_Scripts/PPE_ENS_Defaults_PI.sh /home/ybhatti2/prjs1474/Utils/CDO_Scripts/CDO_PP_PI/Jasper_2006.sh
 
 # Update placeholders inside the copied script in a single sed call
-sed -i     -e "6c jobname='PPE_TEST'"     -e "7c name=\"/home/ybhatti2/prjs1474/Pace_PPE_Output/PI_PPE_Experiments/PPE_TEST\""     "/home/ybhatti2/prjs1474/Utils/CDO_Scripts/CDO_PP_PI/PPE_TEST.sh"
+sed -i     -e "6c jobname='Jasper_2006'"     -e "7c name=\"/home/ybhatti2/prjs1474/Pace_PPE_Output/PI_PPE_Experiments/Jasper_2006\""     "/home/ybhatti2/prjs1474/Utils/CDO_Scripts/CDO_PP_PI/Jasper_2006.sh"
 
 set +e   # allow errors
 module load 2024                                                                                                                                                                                                                    
 module load CDO/2.4.4-gompi-2024a
 module load NCO/5.2.9-foss-2024a
 
-. /home/ybhatti2/prjs1474/Utils/CDO_Scripts/CDO_PP_PI/PPE_TEST.sh
+. /home/ybhatti2/prjs1474/Utils/CDO_Scripts/CDO_PP_PI/Jasper_2006.sh
 
 set -e   # restore strict mode
 # -----------------------------------------------------
 
 # Command line prompt tester
-#jobname="PPE_ENS_1"; #target="/home/ybhatti2/prjs1474/Utils/CDO_Scripts/CDO_PP/PPE_TEST.sh"; #output_dir="/home/ybhatti2/prjs1474/Pace_PPE_Output/PPE_Experiments/PPE_TEST/"; #archive_dir="~/prjs1076/PPE_Output/Pace_PPE_Experiments/PPE_TEST"; #cp -f /home/ybhatti2/prjs1474/Utils/CDO_Scripts/PPE_ENS_Example.sh ""; #sed -i -e "6c jobname='PPE_TEST'" -e "7c name=\"\"" ""
+#jobname="PPE_ENS_1"; #target="/home/ybhatti2/prjs1474/Utils/CDO_Scripts/CDO_PP/Jasper_2006.sh"; #output_dir="/home/ybhatti2/prjs1474/Pace_PPE_Output/PPE_Experiments/Jasper_2006/"; #archive_dir="~/prjs1076/PPE_Output/Pace_PPE_Experiments/Jasper_2006"; #cp -f /home/ybhatti2/prjs1474/Utils/CDO_Scripts/PPE_ENS_Example.sh ""; #sed -i -e "6c jobname='Jasper_2006'" -e "7c name=\"\"" ""
 
 
 flag_p_proc=false                # flag to launch the p-proc
@@ -105,17 +105,17 @@ if $flag_p_proc ; then # this submits a job to be executed on the p-proc machine
                        # you need to declare them as environment variables before
                        # launching the script
 
-   declare -x exp=PPE_TEST
-   declare -x exp_dir=/home/ybhatti2/prjs1474/Pace_PPE_Output/MM_PPE_Experiments/PPE_TEST/
-   declare -x p_proc_dir=/path/to/post_proc_disk/ybhatti2/PPE_TEST
+   declare -x exp=Jasper_2006
+   declare -x exp_dir=/home/ybhatti2/prjs1474/Pace_PPE_Output/MM_PPE_Experiments/Jasper_2006/
+   declare -x p_proc_dir=/path/to/post_proc_disk/ybhatti2/Jasper_2006
 
 #   sbatch -M julier \
 #                       --job-name="p-proc" \
 #                       --nodes=1 \
 #                       --ntasks=1 \
 #                       --time=24:00:00 \
-#                       --output=/gpfs/work3/0/prjs1474/ybhatti/PPE_Project/MM_PPE_ECHAM/my_experiments/PPE_TEST/slurm_p-proc_%j.txt \
-#                       --error=/gpfs/work3/0/prjs1474/ybhatti/PPE_Project/MM_PPE_ECHAM/my_experiments/PPE_TEST/slurm_p-proc_%j.txt \
+#                       --output=/gpfs/work3/0/prjs1474/ybhatti/PPE_Project/MM_PPE_ECHAM/my_experiments/Jasper_2006/slurm_p-proc_%j.txt \
+#                       --error=/gpfs/work3/0/prjs1474/ybhatti/PPE_Project/MM_PPE_ECHAM/my_experiments/Jasper_2006/slurm_p-proc_%j.txt \
 #                       --account="srs25001" \
 #                       --export=ALL \
 #                             /path/to/some/post-processing script
@@ -125,8 +125,8 @@ if $flag_p_proc ; then # this submits a job to be executed on the p-proc machine
                        --nodes=1               \
                        --ntasks=1 \
                        --time=24:00:00 \
-                       --output=/home/ybhatti2/prjs1474/Pace_PPE_Output/MM_PPE_Experiments/PPE_TEST/PPE_TEST.out \
-                       --error=/home/ybhatti2/prjs1474/Pace_PPE_Output/MM_PPE_Experiments/PPE_TEST/PPE_TEST.err \
+                       --output=/home/ybhatti2/prjs1474/Pace_PPE_Output/MM_PPE_Experiments/Jasper_2006/Jasper_2006.out \
+                       --error=/home/ybhatti2/prjs1474/Pace_PPE_Output/MM_PPE_Experiments/Jasper_2006/Jasper_2006.err \
                        --account="srs25001" \
                        --export=ALL \
                              /path/to/some/post-processing script
@@ -144,13 +144,13 @@ module load 2024
 module load netCDF/4.9.2-gompi-2024a
 #<<SF
 
-rerun_echam="restart_PPE_TEST_echam.nc"
+rerun_echam="restart_Jasper_2006_echam.nc"
 
 if [ -e ${rerun_echam} ] ; then
    rerun_date=`ncdump -h ${rerun_echam} | grep ":vdate =" | cut -c12-19`
-   rerun_tar=restart_PPE_TEST_${rerun_date}.tar
+   rerun_tar=restart_Jasper_2006_${rerun_date}.tar
 
-   tar cvf $rerun_tar restart_PPE_TEST_[a-z]*
+   tar cvf $rerun_tar restart_Jasper_2006_[a-z]*
 else
    echo "No rerun file was produced: Don't do any job chaining and stop execution here"
    exit # no need to prepare a potential namelist file for a next submission if there's no rerun file
@@ -166,7 +166,7 @@ cat namelist.echam.bak | sed -e 's/^ *[lL][rR][eE][sS][uU][mM][eE].*$/lresume=.t
 #--- Submit the next job:
 
    if [[ "$status_echam" -eq "0" ]] ; then
-      cd /gpfs/work3/0/prjs1474/ybhatti/PPE_Project/MM_PPE_ECHAM/my_experiments/PPE_TEST
-      sbatch /gpfs/work3/0/prjs1474/ybhatti/PPE_Project/MM_PPE_ECHAM/my_experiments/PPE_TEST/echam_jobscript_PPE_TEST.sh
+      cd /gpfs/work3/0/prjs1474/ybhatti/PPE_Project/MM_PPE_ECHAM/my_experiments/Jasper_2006
+      sbatch /gpfs/work3/0/prjs1474/ybhatti/PPE_Project/MM_PPE_ECHAM/my_experiments/Jasper_2006/echam_jobscript_Jasper_2006.sh
    fi
 
